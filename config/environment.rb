f24f4ffd212fbc509c620ffb80e3839c6cfa287a
KazukiIkushima <rupt30061opt5fx@yahoo.co.jp>
2016-11-16 01:23:20 +0000

    teisei

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
 
-Amazon::Ecs.options = {
-      associate_tag:     'ixykazuki-22',
-      AWS_access_key_id: 'AKIAIUAQBU6AHQOSE76Q',
-      AWS_secret_key:   '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'
-}
 
 # Initialize the Rails application.
 Rails.application.initialize!
